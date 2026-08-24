5c48150bdcc7e49a96973375bef28b9754449aa4
elmarshub <martinsifeanyi247@gmail.com>
2026-08-24 09:52:23 +0100

    security: remove malicious obfuscated payload injected into config/source files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
